a98771e06843c405252e0bf3178f2b00854d96db
EscapeFromJava <reske.artur@gmail.com>
2022-03-30 20:19:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("Старый массив >> " + Arrays.toString(arr));
+        System.out.println("Новый массив >> " + Arrays.toString(GetCumulativeTotal(arr)));
+    }
+
+    public static int[] GetCumulativeTotal(int[] arr) {
+        int[] newArr = new int[arr.length];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            newArr[i] = sum;
+        }
+        return newArr;
+    }
+}
