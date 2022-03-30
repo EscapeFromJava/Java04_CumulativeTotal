997241131c3cd419bc8a29d74d7af9b9b9cb4856
EscapeFromJava <reske.artur@gmail.com>
2022-03-30 20:37:04 +0300

    v 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("Введите кол-во элементов в массиве");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = GetRandomArray(n);
         System.out.println("Старый массив >> " + Arrays.toString(arr));
         System.out.println("Новый массив >> " + Arrays.toString(GetCumulativeTotal(arr)));
     }
 
+    public static int[] GetRandomArray(int n){
+        Random rand = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+            array[i] = rand.nextInt(1000);
+        return array;
+    }
     public static int[] GetCumulativeTotal(int[] arr) {
         int[] newArr = new int[arr.length];
         int sum = 0;
